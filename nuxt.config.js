fe3f8e3fa8e684996fc6e08f2709c723fdc95e51
shunnNet <wendell20904102@gmail.com>
2021-04-21 18:16:32 +0800

    feat: add vue-multi-select

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/scss/all.scss'],
+    'vue-multiselect/dist/vue-multiselect.min.css',
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [

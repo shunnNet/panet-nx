b4469bb568a19d7899f1aed73ca44a43e9b3ead8
shunnNet <wendell20904102@gmail.com>
2021-04-21 18:19:12 +0800

    feat: add loading bar

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,10 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
+  loading: {
+    color: '#ff5722',
+    height: '3px',
+  },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
